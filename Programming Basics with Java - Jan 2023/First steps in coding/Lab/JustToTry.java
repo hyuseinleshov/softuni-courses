c2f526a06e2634021641091967a1941b949e158f
Hiusein <133807047+hiuseinlesho@users.noreply.github.com>
2023-05-19 11:36:14 +0300

    Create JustToTry.java

---
+++ Programming Basics with Java - Jan 2023/First steps in coding/Lab/JustToTry.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class JustToTry {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+
+        System.out.printf("Здравей %s ,как си?",name);
+    }
+}
