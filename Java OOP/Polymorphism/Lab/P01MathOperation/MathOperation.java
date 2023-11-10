21a497fb37c3d7e067bfa28a823ffa274c593b40
Hiusein Lesho <133807047+hiuseinlesho@users.noreply.github.com>
2023-11-10 13:51:35 +0200

    Add P01MathOperation from Polymorphism - Lab

---
+++ Java OOP/Polymorphism/Lab/P01MathOperation/MathOperation.java
@@ -0,0 +1,18 @@
+package Lab.P01MathOperation;
+
+public class MathOperation {
+
+
+    public int add(int a, int b) {
+      return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return add(a, b) + c;
+    }
+
+    public int add(int a, int b, int c, int d) {
+        return add(a, b) + add(c, d);
+    }
+
+}
