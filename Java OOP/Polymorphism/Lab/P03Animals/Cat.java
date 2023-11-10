3266cbb9c6bfabf6e1f21cf4ab4ddbd601ebca80
Hiusein Lesho <133807047+hiuseinlesho@users.noreply.github.com>
2023-11-10 15:19:59 +0200

    Add P03Animals to Polymorphism - Lab

---
+++ Java OOP/Polymorphism/Lab/P03Animals/Cat.java
@@ -0,0 +1,14 @@
+package Lab.P03Animals;
+
+public class Cat extends Animal {
+
+    protected Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return super.explainSelf() + System.lineSeparator() + "MEEOW";
+    }
+
+}
---
+++ Java OOP/Polymorphism/Lab/P03Animals/Dog.java
@@ -0,0 +1,14 @@
+package Lab.P03Animals;
+
+public class Dog extends Animal {
+
+    protected Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return super.explainSelf() + System.lineSeparator() + "DJAAF";
+    }
+
+}
