05a38c918d9fd0b93b220511fc0c56a93c64f9fc
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-01-11 22:38:36 +0200

    Add P01 to Data Definition and Datatypes - Lab

---
+++ Java DB/MySQL/Databases Introduction. Data Definition and Datatypes/Lab/P01CreateTables.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE gamebar;
+
+USE gamebar;
+
+CREATE TABLE employees (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE categories (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    category_id INT NOT NULL
+);
