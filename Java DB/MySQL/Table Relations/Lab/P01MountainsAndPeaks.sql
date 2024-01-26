e44beaf6c290a98b2afee721f5d54f45ff85641a
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-01-26 23:40:26 +0200

    Added problems from Table Relations - Lab

---
+++ Java DB/MySQL/Table Relations/Lab/P01MountainsAndPeaks.sql
@@ -0,0 +1,16 @@
+USE camp;
+
+CREATE TABLE mountains (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE peaks (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    mountain_id INT,
+    
+    CONSTRAINT fk_mountain_id
+    FOREIGN KEY (mountain_id) 
+    REFERENCES mountains(id)
+);
---
+++ Java DB/MySQL/Table Relations/Lab/P04DeleteMountains.sql
@@ -0,0 +1,11 @@
+CREATE TABLE mountains (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE peaks (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    mountain_id INT,
+    FOREIGN KEY (mountain_id) REFERENCES mountains(id) ON DELETE CASCADE
+);
