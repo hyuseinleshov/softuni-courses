288ea87ae89ed3021819aba9e60cb351b9ff71a9
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-01-27 17:17:16 +0200

    Added all problems from Table Relations - Exercise

---
+++ Java DB/MySQL/Table Relations/Exercise/P09PeaksInRila.sql
@@ -0,0 +1,7 @@
+USE geography;
+
+SELECT mountain_range, peak_name, elevation AS peak_elevation
+FROM mountains AS m
+JOIN peaks AS p ON m.id = p.mountain_id
+WHERE mountain_range = 'Rila'
+ORDER BY peak_elevation DESC;
