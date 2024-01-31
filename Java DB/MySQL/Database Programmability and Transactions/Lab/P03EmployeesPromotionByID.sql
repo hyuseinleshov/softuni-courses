dbb6a02262333cf0bd4e7688125bb7d15cc6b8e8
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-01-31 20:15:26 +0200

    Added all problems from Database Programmability and Transactions - Lab

---
+++ Java DB/MySQL/Database Programmability and Transactions/Lab/P03EmployeesPromotionByID.sql
@@ -0,0 +1,24 @@
+DELIMITER $$
+
+CREATE PROCEDURE usp_raise_salary_by_id(id int)
+BEGIN
+START TRANSACTION;
+IF((SELECT count(employee_id) FROM employees WHERE employee_id
+like id)<>1) THEN
+ROLLBACK;
+ELSE
+UPDATE employees AS e SET salary = salary + salary*0.05
+WHERE e.employee_id = id;
+END IF;
+END  $$
+
+DELIMITER ;
+
+SELECT 
+    salary
+FROM
+    employees
+WHERE
+    employee_id = 17;
+    
+CALL usp_raise_salary_by_id(17);
