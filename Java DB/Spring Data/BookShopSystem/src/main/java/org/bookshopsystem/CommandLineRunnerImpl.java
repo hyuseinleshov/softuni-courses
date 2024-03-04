42794e8653bd57c6ee61a3c10c24401fa0c336b9
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-03-04 11:47:14 +0200

    Added all from Bookshop System - Exercise

---
+++ Java DB/Spring Data/BookShopSystem/src/main/java/org/bookshopsystem/CommandLineRunnerImpl.java
@@ -0,0 +1,58 @@
+package org.bookshopsystem;
+
+import org.bookshopsystem.service.AuthorService;
+import org.bookshopsystem.service.BookService;
+import org.bookshopsystem.service.CategoryService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+    private final AuthorService authorService;
+    private final CategoryService categoryService;
+    private final BookService bookService;
+
+    public CommandLineRunnerImpl(AuthorService authorService, CategoryService categoryService, BookService bookService) {
+        this.authorService = authorService;
+        this.categoryService = categoryService;
+        this.bookService = bookService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        seedData();
+//        getAllBooksAfterYear2000();
+//        getAuthorsFirstLastNameBeforeBooks1990();
+//        getAllAuthorsByBookDesc();
+        printBooksByGeorgePowell();
+    }
+
+    private void printBooksByGeorgePowell() {
+        this.bookService.findAllBooksByGeorgePowellOrdered()
+                .forEach(System.out::println);
+    }
+
+    private void getAllAuthorsByBookDesc() {
+        this.authorService.getAllAuthorsDescBooks()
+                .forEach(System.out::println);
+    }
+
+    private void getAuthorsFirstLastNameBeforeBooks1990() {
+        this.authorService.getAllAuthorsFirstAndLastNameForBooksBeforeYear1990()
+                .forEach(System.out::println);
+    }
+
+    private void getAllBooksAfterYear2000() {
+        this.bookService.findAllBooksAfterYear2000()
+                .forEach(System.out::println);
+    }
+
+    private void seedData() throws IOException {
+        this.categoryService.seedCategories();
+        this.authorService.seedAuthors();
+        this.bookService.seedBooks();
+    }
+}
---
+++ Java DB/Spring Data/BookShopSystem/src/main/java/org/bookshopsystem/service/BookService.java
@@ -0,0 +1,13 @@
+package org.bookshopsystem.service;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface BookService {
+
+    void seedBooks() throws IOException;
+
+    List<String> findAllBooksAfterYear2000();
+
+    List<String> findAllBooksByGeorgePowellOrdered();
+}
