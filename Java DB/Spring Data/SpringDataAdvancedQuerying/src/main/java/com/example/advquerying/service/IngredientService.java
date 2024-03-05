595f0d5854445db55eaa94e7f504f4548f5aa053
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-03-05 17:19:41 +0200

    Added all from /Spring Data Advanced Querying - Lab

---
+++ Java DB/Spring Data/SpringDataAdvancedQuerying/src/main/java/com/example/advquerying/service/IngredientService.java
@@ -0,0 +1,15 @@
+package com.example.advquerying.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface IngredientService {
+
+    List<String> getAllIngredientWithStartingName(String symbol);
+
+    int deleteIngredientByName(String name);
+
+    int updatedIngredientPrices();
+
+    int updatePricesForGivenNames();
+}
