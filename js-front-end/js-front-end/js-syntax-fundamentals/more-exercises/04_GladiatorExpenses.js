3733789d022720f9b0e4abcd994b259fe34d65b2
hyuseinleshov <hyusein.leshov@gmail.com>
2024-11-03 08:22:30 +0200

    Add exercise 4 and 5 from JS Syntax Fundamentals - More Exercises

--- js-front-end/js-front-end/js-syntax-fundamentals/more-exercises/04_GladiatorExpenses.js
+++ js-front-end/js-front-end/js-syntax-fundamentals/more-exercises/04_GladiatorExpenses.js
@@ -1,5 +1,39 @@
-function solve() {
+function solve(lostFightsCount, helmetPrice, swordPrice, shieldPrice, armorPrice) {
+    let brokenHelmetCount = 0;
+    let brokenSwordCount = 0;
+    let brokenShieldCount = 0;
+    let brokenArmorCount = 0;
+    
+    while (lostFightsCount > 0) {
+        let brokenItems = 0;
 
+        if (lostFightsCount % 2 == 0) {
+            brokenHelmetCount++;
+            brokenItems++;
+        }
+        
+        if (lostFightsCount % 3 == 0) {
+            brokenSwordCount++;
+            brokenItems++;
+        }
+
+        if (brokenItems == 2) {
+            brokenShieldCount++;
+
+            if (brokenShieldCount % 2 == 0) {
+                brokenArmorCount++;
+            }
+        }
+
+        lostFightsCount--;
+    }
+
+    const totalExpenses = (brokenHelmetCount * helmetPrice)
+        + (brokenSwordCount * swordPrice)
+        + (brokenShieldCount * shieldPrice)
+        + (brokenArmorCount * armorPrice);
+
+    console.log(`Gladiator expenses: ${totalExpenses.toFixed(2)} aureus`);
 }
 
 solve(7, 2, 3, 4, 5);                // Gladiator expenses: 16.00 aureus
--- js-front-end/js-front-end/js-syntax-fundamentals/more-exercises/05_SpiceMustFlow.js
+++ js-front-end/js-front-end/js-syntax-fundamentals/more-exercises/05_SpiceMustFlow.js
@@ -1,8 +1,23 @@
-function solve() {
+function solve(startingYield) {
+    let totalAmount = 0;
+    let days = 0;
 
+    let yield = startingYield;
+    while (yield >= 100) {
+        totalAmount += yield - 26;
+        yield -= 10;
+        days++;
+    }
+
+    if (days > 0) {
+        totalAmount -= 26;
+    }
+
+    console.log(days);
+    console.log(totalAmount);
 }
 
-solve(111);
+solve(98);
 // 2
 // 134
 solve(450);
